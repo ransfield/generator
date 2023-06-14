d3d983fcb568540da1169b31572f196d78b24cb6
Alex Troy Ransfield <Ransfield@Alexs-MacBook-Pro.local>
2023-06-15 00:31:00 +0200

    adding working files

---
+++ script.js
@@ -0,0 +1,37 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("randomiser");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function generateRandomColor() {
+  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
+}
+
+function setRandomColors() {
+  color1.value = generateRandomColor();
+  color2.value = generateRandomColor();
+  setGradient();
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+setGradient();
+
+button.addEventListener("click", setRandomColors);
+
